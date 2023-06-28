0b3beb5a5839e38b0701638f63a66e10c25100da
Eros Koller <eros.koller@7comm.com.br>
2023-06-28 08:35:21 -0300

    change to pepe the frog

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 # nftpepe
-=======
-# meunft
->>>>>>> 6ca9c34 (Initial commit)
+
--- pepethefrog.sol
+++ pepethefrog.sol
@@ -8,14 +8,20 @@ import "@openzeppelin/contracts@4.9.2/access/AccessControl.sol";
 import "@openzeppelin/contracts@4.9.2/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts@4.9.2/utils/Counters.sol";
 
-contract CorsaPiquet is ERC721, ERC721URIStorage, Pausable, AccessControl, ERC721Burnable {
+contract PepeTheFrog is
+    ERC721,
+    ERC721URIStorage,
+    Pausable,
+    AccessControl,
+    ERC721Burnable
+{
     using Counters for Counters.Counter;
 
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("Corsa Piquet", "CPQT") {
+    constructor() ERC721("Pepe The Frog", "PPFG") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
@@ -29,24 +35,31 @@ contract CorsaPiquet is ERC721, ERC721URIStorage, Pausable, AccessControl, ERC72
         _unpause();
     }
 
-    function safeMint(address to, string memory uri) public onlyRole(MINTER_ROLE) {
+    function safeMint(address to, string memory uri)
+        public
+        onlyRole(MINTER_ROLE)
+    {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
-        internal
-        whenNotPaused
-        override
-    {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override whenNotPaused {
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
         super._burn(tokenId);
     }
 
